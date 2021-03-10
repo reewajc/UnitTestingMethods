b0d69cfb9535368ece3b069be3a01750c04f2d35
reewajc <37374398+reewajc@users.noreply.github.com>
2021-03-09 22:04:11 -0600

    Add project files.

---
+++ ConsoleTesting.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleTesting
+{
+    class ConsoleTesting
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
---
+++ Number.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace ConsoleTesting
+{
+  public   class Number
+    {
+        /// <summary>
+        /// This method adds list of numbers
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <returns>int</returns>
+     public  int AddNumbers(params int[] numbers)
+        {
+            int result = 0;
+            foreach(int number in numbers)
+            {
+                result += number;
+
+            }
+            return result;
+        }
+        /// <summary>
+        /// This method add up list of double numbers
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <returns>double</returns>
+        public double AddNumbers(params double[] numbers)
+        {
+            double result = 0;
+            foreach (double number in numbers)
+            {
+                result += number;
+
+            }
+            return result;
+        }
+        /// <summary>
+        /// This method multiply  list of double numbers
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <returns>double</returns>
+        public double MultiplyNumbers(params double[] numbers)
+        {
+            double result = 1;
+            foreach (double number in numbers)
+            {
+                result *= number;
+
+            }
+            return result;
+        }
+        /// <summary>
+        /// This method multiply list of numbers
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <returns>int</returns>
+        public  int MultiplyNumbers(params int[] numbers)
+        {
+            int result = 1;
+            foreach (int number in numbers)
+            {
+                result *= number;
+
+            }
+            return result;
+        }
+    }
+}
